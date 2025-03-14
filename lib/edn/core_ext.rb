c54f329a0102c806a7d8fefe73eedf96e891a2b8
David Verbenyi <dverbenyi@gmail.com>
2025-03-14 08:48:33 +0000

    fix: Update Bignum to Integer for Ruby 3 support
    - Updated Bignum to Integer to allow Ruby 3 support in core_ext.rb

--- lib/edn/core_ext.rb
+++ lib/edn/core_ext.rb
@@ -94,7 +94,7 @@ module EDN
 end
 
 Numeric.send(:include, EDN::CoreExt::Unquoted)
-Bignum.send(:include, EDN::CoreExt::Bignum)
+Integer.send(:include, EDN::CoreExt::Bignum)
 BigDecimal.send(:include, EDN::CoreExt::BigDecimal)
 TrueClass.send(:include, EDN::CoreExt::Unquoted)
 FalseClass.send(:include, EDN::CoreExt::Unquoted)
